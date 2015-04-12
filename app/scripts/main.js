1cc00b3aa053d0f703e741dcf2a6b5ed7a0d4689
Clayton Albachten <clay.reed.a@gmail.com>
2015-04-12 13:49:16 -0400

    Rerender board rather than reload page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,31 +8,8 @@ window.Memory = {
     Events: {},
     init: function() {
         'use strict';
-        var allLetters = [
-            'α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η',
-            'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ', 
-            'ο', 'π', 'ρ', 'σ', 'τ', 'υ', 'φ', 
-            'χ', 'ψ', 'ω'
-        ], 
-                // r, o, y, g, b, p 
-            colors = ['#FC1501', '#FF7F00', '#FFE600', 
-                      '#0e1', '#0BB5FF', '#C0F'];
-
-
-
-        var letters = _.sample(allLetters, 6), 
-            cards = new Memory.Collections.Cards();
-
-        _.each(letters, function(letter, idx){
-            var attrs         = {cardValue: letter,
-                                 cardColor: colors[idx]}; 
-            var cardPairFirst = new Memory.Models.Card(attrs),  
-               cardPairSecond = new Memory.Models.Card(attrs);
-            
-            cards.add([cardPairFirst, cardPairSecond]);
-        });
-
-        var boardView = new Memory.Views.Board({collection: cards}); 
+        
+        var boardView = new Memory.Views.Board(); 
         $('.game').html(boardView.render().$el);
         
     }
--- app/scripts/views/board.js
+++ app/scripts/views/board.js
@@ -58,6 +58,7 @@ Memory.Views = Memory.Views || {};
       },
 
       seeIfWon: function(){
+        var that = this; 
         if (_.every(this.collection.models, function(model){
           return model.get('isMatched');
         })) {
@@ -68,7 +69,9 @@ Memory.Views = Memory.Views || {};
             .css('margin-top', '2em');
           console.log('You won in '+this.moves+' moves!');
           setTimeout(function(){
-            window.location.reload(false);
+            that.render();
+            $('header h1').text('Memory')
+              .css('margin-top', '0');
           }, 1500);
 
         } 
@@ -78,8 +81,38 @@ Memory.Views = Memory.Views || {};
         this.listenTo(Memory.Events, 'UncoveredCardEvent', this.addUncoveredCard);
       },
 
+      createCards: function(){
+        var allLetters = [
+            'α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η',
+            'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ', 
+            'ο', 'π', 'ρ', 'σ', 'τ', 'υ', 'φ', 
+            'χ', 'ψ', 'ω'
+        ], 
+                // r, o, y, g, b, p 
+            colors = ['#FC1501', '#FF7F00', '#FFE600', 
+                      '#0e1', '#0BB5FF', '#C0F'];
+
+        var letters = _.sample(allLetters, 6), 
+            cards = new Memory.Collections.Cards();
+
+        _.each(letters, function(letter, idx){
+          var attrs         = {cardValue: letter,
+                                 cardColor: colors[idx]}; 
+          var cardPairFirst = new Memory.Models.Card(attrs),  
+             cardPairSecond = new Memory.Models.Card(attrs);
+            
+          cards.add([cardPairFirst, cardPairSecond]);
+        });
+
+        this.collection = cards; 
+
+      },
+
       render: function () {
         var that = this; 
+
+        this.createCards();
+
         this.$el.html(this.template());
 
           _.each(_.shuffle(this.collection.models), function(card){
